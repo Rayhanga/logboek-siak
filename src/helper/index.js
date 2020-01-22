3ba0c42d67577bb27d93193833a14c241ce0171a
Rayhan Gusty Alif <rayhangustyalif@gmail.com>
2020-01-22 10:33:53 +0700

    Add additional helper from external sources

--- src/helper/index.js
+++ src/helper/index.js
@@ -15,3 +15,21 @@ export const fetcher = async (uri, method, payload) => {
 
   return response
 }
+
+export const formatDate = (date) => {
+  var d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+  if (month.length < 2) 
+      month = '0' + month;
+  if (day.length < 2) 
+      day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
+export const numberSep = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
