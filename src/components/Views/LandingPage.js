0bb253fac316e1d7870fa05b5bd8506dd5955c05
Rayhan Gusty Alif <rayhangustyalif@gmail.com>
2020-01-27 06:23:29 +0700

    Add placeholder for Stok Barang chart

--- src/components/Views/LandingPage.js
+++ src/components/Views/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useEffect } from 'react'
+import React, { useMemo } from 'react'
 import { Chart } from 'react-charts'
 
 export default (props) => {
@@ -6,25 +6,25 @@ export default (props) => {
         <div className="container">
             <h1>Halaman Utama</h1>
             <div className="row">
-                <div className="col">
-                    <Item title="Saldo Akun Kas" dataset={props.data.akun}/>
+                <div className="col m-3">
+                    <ChartSaldo title="Saldo Akun Kas" dataset={props.data.akun}/>
                 </div>
-                {/* <div className="col-4">
-                    <Item title=""/>
-                </div> */}
+                <div className="col m-3">
+                    <ChartBarang title="Stok Barang" dataset={props.data.barang}/>
+                </div>
             </div>
         </div>
     )
 }
 
-const Item = (props) => {
+const ChartSaldo = (props) => {
     const { dataset } = props.dataset ? props : () => {}
 
     const lut = [
         'D', 'K', 'K'
     ]
 
-    const dataSaldo = useMemo(() => {
+    const chartData = useMemo(() => {
         const res = []
         var x, sum = 0
         for(x in dataset){
@@ -45,8 +45,6 @@ const Item = (props) => {
             data: res
         }]
     })
-
-    // console.log(dataSaldo)
 
     const series = useMemo(
         () => ({
@@ -75,8 +73,73 @@ const Item = (props) => {
                         height: '30vh'
                     }}
                 >
+                {props.tipe === 'saldo'}
                     <Chart 
-                        data={dataSaldo}
+                        data={chartData}
+                        axes={axes} 
+                        series={series}
+                        tooltip/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const ChartBarang = (props) => {
+    const { dataset } = props.dataset ? props : () => {}
+
+    const chartData = useMemo(() => {
+        // const res = []
+        // var x, sum = 0
+        // for(x in dataset){
+        //     var y = 0, sum = 0
+        //     for(y in dataset[x].details){
+        //         sum = dataset[x].details[y].dk === lut[dataset[x].ref.charAt(0)-1] ? sum + dataset[x].details[y].nominal : sum - dataset[x].details[y]
+        //     }
+        //     sum = isNaN(sum) ? 0 : sum
+        //     let foo
+        //     dataset[x].ref.charAt(0) === '1'
+        //     ? res.push([
+        //         dataset[x].nama, sum
+        //     ])
+        //     : foo = NaN
+        // }
+        return [{
+            label: 'Saldo Akhir',
+            data: [0,5]
+        }]
+    })
+
+    const series = useMemo(
+        () => ({
+        type: 'bar'
+      }),
+      []
+    )
+     
+    const axes = useMemo(
+    () => [
+        { primary: true, type: 'ordinal', position: 'bottom' },
+        { type: 'linear', position: 'left' }
+    ],
+    []
+    )
+
+    return(
+        <div className="card">
+            <div className="card-header">
+                <h3>{props.title}</h3>
+            </div>
+            <div className="card-body">
+                <div
+                    className="m-3"
+                    style={{
+                        height: '30vh'
+                    }}
+                >
+                {props.tipe === 'saldo'}
+                    <Chart 
+                        data={chartData}
                         axes={axes} 
                         series={series}
                         tooltip/>
