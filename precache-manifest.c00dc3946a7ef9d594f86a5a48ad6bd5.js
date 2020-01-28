091724d30d555f8185705ec9dbd0863753f69ba2
Rayhan Gusty Alif <rayhangustyalif@gmail.com>
2020-01-28 14:53:18 +0700

    Updates

---
+++ precache-manifest.c00dc3946a7ef9d594f86a5a48ad6bd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe12a07c0587a69441ce27e1cd25dde1",
+    "url": "/logboek-siak/index.html"
+  },
+  {
+    "revision": "b126a34dbc783ad09a65",
+    "url": "/logboek-siak/static/css/main.09a5bf13.chunk.css"
+  },
+  {
+    "revision": "ab40702d8baa99a52de1",
+    "url": "/logboek-siak/static/js/2.63eacb2d.chunk.js"
+  },
+  {
+    "revision": "8f7a21fee2f3ed58528da483e7db29e3",
+    "url": "/logboek-siak/static/js/2.63eacb2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b126a34dbc783ad09a65",
+    "url": "/logboek-siak/static/js/main.17173e0c.chunk.js"
+  },
+  {
+    "revision": "87d2b7989695e942fd35",
+    "url": "/logboek-siak/static/js/runtime-main.04d52ab0.js"
+  },
+  {
+    "revision": "72e05492adcce98edd298fee8e2945fb",
+    "url": "/logboek-siak/static/media/brand.72e05492.png"
+  }
+]);
